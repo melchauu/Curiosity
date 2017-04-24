964b664c1616a7b14840896c35866484df5e670b
melchauu <mel_o_di@hotmail.com>
2017-04-24 17:43:08 -0400

    added links to js scripts

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,5 @@
 app.controller('MainController', ['$scope', 'marsPhotosMain', function($scope, marsPhotosMain) {
   marsPhotosMain.success(function(data) {
-    $scope.fiveDay = data;
+    $scope.mastCam = data;
   });
 }]);
--- js/services/marsPhotosMain.js
+++ js/services/marsPhotosMain.js
@@ -1,11 +1,30 @@
 app.factory('marsPhotosMain', ['$http', function($http) { 
+	var url = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=mast&api_key=DEMO_KEY";
+	$http({
+		method: 'JSONP',
+		url: url
+	}).
+	success(function(status) {
+		var img = data.photos[0].img_src;
+		console.log(img);
+		return img;
+	}).
+	error(function(status) {
+		return err;
+	})
+}]);
+
+
+
+/*app.factory('marsPhotosMain', ['$http', function($http) { 
   return $http.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=mast&api_key=DEMO_KEY') 
             .success(function(data) {
-				var img = data.photos;
-				
-              return data;
+				var img = data.photos[0].img_src;
+				console.log(img);
+              return img;
             }) 
             .error(function(err) { 
               return err;
             }); 
 }]);
+*/
