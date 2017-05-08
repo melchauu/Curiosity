eb4053a4ee6b354e3c15d92a6a436ce87affc911
melchauu <mel_o_di@hotmail.com>
2017-05-08 16:55:43 -0400

    setting up and researching get request

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,9 @@
+var https = require('https');
+
+
+req.on('error', function(e) {
+  console.log('ERROR: ' + e.message);
+});
 /*var Todo = require('./models/todo');
 
 function getTodos(res) {
@@ -19,6 +25,30 @@ module.exports = function (app) {
     app.get('/api/todos', function (req, res) {
         // use mongoose to get all todos in the database
         //getTodos(res);
+		
+		/*var options = {
+		  host: 'www.google.com',
+		  path: '/index.html'
+		};*/
+
+		var req = https.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=mast&api_key=DEMO_KEY', function(res) {
+		  console.log('STATUS: ' + res.statusCode);
+		  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+		  // Buffer the body entirely for processing as a whole.
+		  var bodyChunks = [];
+		  res.on('data', function(chunk) {
+			// You can process streamed parts here...
+			bodyChunks.push(chunk);
+		  }).on('end', function() {
+			var body = Buffer.concat(bodyChunks);
+			console.log('BODY: ' + body);
+			// ...and/or process the entire body here.
+		  })
+		});
+		req.on('error', function(e) {
+		  console.log('ERROR: ' + e.message);
+		});
 		console.log("get all todos was called");
     });
 
