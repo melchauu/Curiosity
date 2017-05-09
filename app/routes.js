bac0f34b7f9f864c08e65979544717e24f9db87c
melchauu <mel_o_di@hotmail.com>
2017-05-08 21:39:22 -0400

    rearrange route

--- app/routes.js
+++ app/routes.js
@@ -1,31 +1,11 @@
 var https = require('https');
 
-
-req.on('error', function(e) {
-  console.log('ERROR: ' + e.message);
-});
-/*var Todo = require('./models/todo');
-
-function getTodos(res) {
-    Todo.find(function (err, todos) {
-
-        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(todos); // return all todos in JSON format
-    });
-};*/
-
 module.exports = function (app) {
 
     // api ---------------------------------------------------------------------
     // get all todos
     app.get('/api/todos', function (req, res) {
-        // use mongoose to get all todos in the database
-        //getTodos(res);
-		
+
 		/*var options = {
 		  host: 'www.google.com',
 		  path: '/index.html'
