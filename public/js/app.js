6b73dc5e70b77ac62a7bfd7cf575fadec639f391
melchauu <mel_o_di@hotmail.com>
2017-07-02 00:04:07 -0400

    sol watcher created

--- public/js/app.js
+++ public/js/app.js
@@ -3,27 +3,37 @@ angApp.controller('MainController', ['$scope', 'marsPhotosMain','$http', functio
 		$scope.formData = {};
 		$scope.loading = true;
 
-		$scope.sol = 0;
+		$scope.sol = 1;
 		$scope.solDecr = function(){
-			if ($scope.sol > 0){
+			if ($scope.sol > 1){
 				$scope.sol--;
 			}
 		};
-		marsPhotosMain.get()
-		.success(function(data) {
-			$scope.mastCam = data;
-			console.log("Success: "+ data);
-		})
-		.error(function(data) {
-				console.log('Error: ' + data);
-			});
 
+		$scope.$watch('sol', function(newVal, oldVal){
+	    console.log("Sol was changed to:"+newVal);
+	    $scope.sol.watch = newVal;
+
+			marsPhotosMain.get($scope.sol)
+			.success(function(data) {
+				if (data == ""){
+					$scope.mastCam = "img/photoNotFound.png"
+				}
+				else{
+					$scope.mastCam = data;
+				}
+				console.log("Success: "+ data);
+			})
+			.error(function(data) {
+					console.log('Error: ' + data);
+				});
+	  });
 	}]);
 
 angApp.factory('marsPhotosMain', ['$http', function($http) {
   return {
-  			get : function() {
-  				return $http.get('/api/todos');
+  			get : function(sol) {
+  				return $http.get('/api/todos/' + sol);
   			},
   			create : function(todoData) {
   				return $http.post('/api/todos', todoData);
--- public/routes.js
+++ public/routes.js
@@ -4,15 +4,20 @@ module.exports = function (app) {
 
     // api ---------------------------------------------------------------------
     // get all todos
-    app.get('/api/todos', function (req, res) {
+    app.get('/api/todos/:sol_num', function (req, res) {
 
 		/*var options = {
 		  host: 'www.google.com',
 		  path: '/index.html'
 		};*/
 		var returnData;
-		var req = https.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=mast&api_key=DEMO_KEY', function(reshttp) {
+    var solNum = req.params.sol_num;
+    var nasaAPIKey = 'abhkFZppS4xqHplsJAV5kBJfqVcsqtbYxvLBvYB5';
+    var getURL = ('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol='+ (parseInt(solNum, 10)+50) + '&camera=mast&api_key=' + nasaAPIKey);
+    console.log('SOLNUM =' + solNum);
+		var req = https.get(getURL, function(reshttp) {
 		  console.log("get all todos was called");
+      console.log("getURL: "+ getURL)
 		  //console.log('STATUS: ' + reshttp.statusCode);
 		  //console.log('HEADERS: ' + JSON.stringify(reshttp.headers));
 
@@ -24,12 +29,20 @@ module.exports = function (app) {
 		  }).on('end', function() {
 			var body = Buffer.concat(bodyChunks);
 			var jsonBody = JSON.parse(body);
-			console.log('BODY: ' + JSON.stringify(jsonBody.photos[0].img_src));
+      //console.log('INIT BODY: ' + JSON.stringify(jsonBody));
+      console.log('SOLNUM: ' + solNum);
+      if (jsonBody.error || jsonBody.photos.length == 0){
+        returnData = ""
+      }
+      else{
+        returnData = (jsonBody.photos[0].img_src)
+      }
+			console.log('BODY: ' + JSON.stringify(returnData));
 			//reshttp.json(jsonBody.photos[0].img_src);
-			// ** the args in .get and .post are special. 
+			// ** the args in .get and .post are special.
 			//req is the request, res is the result
-			//must call res.send or res.json to actually trigger the callback, and finish this get request. 
-			res.send(jsonBody.photos[0].img_src);
+			//must call res.send or res.json to actually trigger the callback, and finish this get request.
+			res.send(returnData);
 			// ...and/or process the entire body here.
 		  })
 		});
