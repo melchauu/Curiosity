c35d0f3c092b368e5e99ad495151e25c206b5d82
melchauu <mel_o_di@hotmail.com>
2017-07-04 15:22:41 -0400

    set sol from input box

--- public/js/app.js
+++ public/js/app.js
@@ -2,14 +2,25 @@ var angApp = angular.module('curiosity', []);
 angApp.controller('MainController', ['$scope', 'marsPhotosMain','$http', function($scope, marsPhotosMain,$http) {
 		$scope.formData = {};
 		$scope.loading = true;
-
 		$scope.sol = 1;
+		$scope.solDisplay = 'Sol ' + $scope.sol;
 		$scope.solDecr = function(){
 			if ($scope.sol > 1){
 				$scope.sol--;
+				$scope.solDisplay = 'Sol ' + $scope.sol;
 			}
 		};
-
+		$scope.solIncr = function(){
+				$scope.sol++;
+				$scope.solDisplay = 'Sol ' + $scope.sol;
+		};
+		$scope.solDisplayChanged = function(){
+			var extractedSol = $scope.solDisplay.replace ( /[^\d.]/g, '' );
+			if (extractedSol != null && extractedSol > 0) {
+				$scope.sol = extractedSol;
+				$scope.solDisplay = 'Sol ' + $scope.sol;
+			}
+		};
 		$scope.$watch('sol', function(newVal, oldVal){
 	    console.log("Sol was changed to:"+newVal);
 	    $scope.sol.watch = newVal;
