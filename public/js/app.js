b04168157302cf904aed0f1178f8c713a212ac5a
melchauu <mel_o_di@hotmail.com>
2017-07-04 17:08:02 -0400

    hours functions ipr

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,47 @@ var angApp = angular.module('curiosity', []);
 angApp.controller('MainController', ['$scope', 'marsPhotosMain','$http', function($scope, marsPhotosMain,$http) {
 		$scope.formData = {};
 		$scope.loading = true;
+
 		$scope.sol = 1;
-		$scope.solDisplay = 'Sol ' + $scope.sol;
+		$scope.solDispUpdate = function(){return 'Sol '  + $scope.sol;}
+		$scope.solDisplay = $scope.solDispUpdate();
+
+		$scope.hrs = 0;
+		$scope.hrsDispUpdate = function(hours){
+			return ((hours < 10)?("0" + hours + ":00"):(hours + ":00"));
+		};
+		$scope.hrsDisplay = $scope.hrsDispUpdate($scope.hrs);
 		$scope.solDecr = function(){
 			if ($scope.sol > 1){
 				$scope.sol--;
-				$scope.solDisplay = 'Sol ' + $scope.sol;
+				$scope.solDisplay = $scope.solDispUpdate();
 			}
 		};
 		$scope.solIncr = function(){
 				$scope.sol++;
-				$scope.solDisplay = 'Sol ' + $scope.sol;
+				$scope.solDisplay = $scope.solDispUpdate();
 		};
+
 		$scope.solDisplayChanged = function(){
 			var extractedSol = $scope.solDisplay.replace ( /[^\d.]/g, '' );
 			if (extractedSol != null && extractedSol > 0) {
 				$scope.sol = extractedSol;
-				$scope.solDisplay = 'Sol ' + $scope.sol;
+				$scope.solDisplay = $scope.solDispUpdate();
 			}
 		};
+
+		$scope.hrsUpdate = function(offset){
+			console.log("offset is "+ offset);
+			console.log("hrs is "+ $scope.hrs);
+			$scope.hrs= $scope.hrs + parseInt(offset);
+			console.log("hrs is now "+ $scope.hrs);
+			if ($scope.hrs > 23 ){
+				$scope.sol++;
+				$scope.solDisplay=$scope.solDispUpdate();
+				$scope.hrs = 0;
+			}
+			$scope.hrsDisplay=$scope.hrsDispUpdate($scope.hrs);
+		}
 		$scope.$watch('sol', function(newVal, oldVal){
 	    console.log("Sol was changed to:"+newVal);
 	    $scope.sol.watch = newVal;
